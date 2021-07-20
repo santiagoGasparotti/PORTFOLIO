2333cc346fa03065f67a17e3e922c50fc4ce7960
Santiago Gasparotti <santiago.gasparotti@hotmail.com>
2021-07-20 11:03:04 -0300

    add js

---
+++ js/index.js
@@ -0,0 +1,22 @@
+// let CUPONPROMO = 15;
+
+// let ingresoCupon = prompt("Ingresa tu cupon");
+
+// let operacion1 = "Con " + ingresoCupon + " ya te aplicamos un " + CUPONPROMO + "% de descuento. Que lo disfrutes!";
+// alert(operacion1);
+
+// EJEMPLO CARNET
+
+let persona = "SANTIAGO GASPAROTTI"
+let domicilio = "ARTURO ALIO 1234"
+let pais = "ARG"
+let nacimiento = "05/11/1996"
+const codigo = "39873433";
+
+let carnet = "Codigo: "+codigo+ " "+
+            "Nombre: "+persona+ " "+
+            "Pais: "+pais+ " "+
+            "Nacimiento: "+nacimiento+ " "+
+            "Domicilio: "+domicilio;
+
+console.log(carnet)
